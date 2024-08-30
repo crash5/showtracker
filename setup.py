c008bf3cc20a2141b7e6ea6c1d7dc2ecf697695c
Imre NAGY <crash@veluna.eu>
2024-08-30 15:50:29 +0200

    Remove old code

--- setup.py
+++
@@ -1,4 +0,0 @@
-# TODO: This shim delegates the job of doing an editable install to Setuptools’ legacy mechanism until native support for PEP 660 is available.
-
-from setuptools import setup
-setup()
