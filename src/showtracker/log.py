e7dab991104fed2dbcc90b80b7456692422aea50
Imre NAGY <crash@veluna.eu>
2024-09-04 06:33:59 +0200

    Log info level to stdout too

--- src/showtracker/log.py
+++ src/showtracker/log.py
@@ -1,4 +1,5 @@
 import logging
+from logging import StreamHandler
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
@@ -7,6 +8,7 @@ def init_app(app):
     lf = logging.Formatter(
         fmt="%(asctime)s [%(levelname)-8s] %(message)s [logger=%(name)s process_id=%(process)d file=%(filename)s line=%(lineno)d]"
     )
+
     logpath = Path(app.instance_path)
     logpath.mkdir(parents=True, exist_ok=True)
     file_handler = RotatingFileHandler(
@@ -14,6 +16,11 @@ def init_app(app):
     )
     file_handler.setFormatter(lf)
     file_handler.setLevel(logging.DEBUG)
+
+    stream_handler = StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
     root.addHandler(file_handler)
+    root.addHandler(stream_handler)
